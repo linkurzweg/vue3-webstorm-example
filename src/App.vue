46003223d532601358290d5c5b1a2b92a00ae4c2
Lin <lin.kurzweg@iti-software.de>
2022-04-25 16:34:42 +0200

    feat: rename HelloWorld component to create a reproducible example for SonarLint

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Slots from "./components/Slots.vue";
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <hello-world-component msg="Hello Vue 3 + TypeScript + Vite" />
   <div class="slots">
     <Slots />
   </div>
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorldComponent from './components/HelloWorld.vue'
+import Slots from "./components/Slots.vue";
+</script>
+
 
 <style>
 #app {
