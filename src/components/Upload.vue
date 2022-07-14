618e3c85338c1b15523f3369fa32824c66435a23
Lin <lin.kurzweg@iti-software.de>
2022-07-14 10:22:29 +0200

    feat: update packages and create reproducible example for NaiveUI failing build

---
+++ src/components/Upload.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-upload
+      action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
+      :headers="{
+      'naive-info': 'hello!'
+    }"
+      :data="{
+      'naive-data': 'cool! naive!'
+    }"
+      :on-error="onError"
+  >
+    <n-button>Upload File</n-button>
+  </n-upload>
+</template>
+
+<script setup lang="ts">
+import { NUpload } from 'naive-ui'
+
+const onError = () => {
+  console.log('Upload failed!')
+}
+</script>
+
+<style scoped lang="sass"></style>
