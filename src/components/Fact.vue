b10722ce987d59ae432b29023bf123a3deddb5f5
Lin <lin.kurzweg@iti-software.de>
2022-10-27 12:55:33 +0200

    feat: add example for false-positive type warning

--- src/components/Fact.vue
+++ src/components/Fact.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ data: { fact: string, length: number}}>()
+const props = defineProps<{ data?: { fact: string, length: number}}>()
 </script>
 
 <style scoped lang="sass"></style>
