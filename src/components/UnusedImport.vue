51197011a60121f76d66f5a0438b60d42535951b
Lin <lin.kurzweg@iti-software.de>
2023-08-10 12:58:07 +0200

    feat: add example for unused import bug in WebStorm

---
+++ src/components/UnusedImport.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import  HelloWorld  from './HelloWorld.vue'
+
+
+
+const helloWorld = ref('Hello World')
+
+</script>
+
+<template>
+  <HelloWorld />
+  <span>{{ helloWorld }}</span>
+</template>
--- src/components/VForBug.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import {ref} from 'vue'
-const testArr = ref([1, 2, 3, 4, 5, 6, 7, 8, 9])
-</script>
-
-<template>
-  <div v-for="entry in testArr.filter(entry => entry < 10)">{{ entry }}</div>
-</template>
